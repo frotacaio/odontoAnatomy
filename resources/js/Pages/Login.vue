ff1bed0ce59f087c0a261a24f4c90d1a364d78ef
frotacaio <caiofrota@alu.ufc.br>
2023-07-25 13:00:17 -0300

    Instalando pacotes e reformulando estrutura de codigo para rodar projeto no docker

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -40,7 +40,6 @@
 				</div>
 				<button type="submit" class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
 				<Link as="button" href="/Registro" class="block w-full bg-white mt-3 py-1 rounded-2xl text-indigo-600 border-2 border-indigo-600 font-semibold mb-2" >Registrar</Link>
-				<span class="text-sm ml-2 text-teal-500 hover:text-teal-700 cursor-pointer flex justify-center">Esqueceu a senha?</span>
 			</form>
 		</div>
 	</div>
